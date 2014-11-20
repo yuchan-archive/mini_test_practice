fab5cfcedcad41ed9e8ebe5684d0650f88641538
Yusuke Ohashi <yusuke@junkpiano.me>
2014-11-20 21:24:02 +0900

    polish test cases.

--- test/test_mini_test_practice.rb
+++ test/test_mini_test_practice.rb
@@ -13,32 +13,25 @@ class TestMiniTestPractice < MiniTest::Test
       assert_equal(MiniTestPractice::Calculator.odd?(333), true)
   end
 
-  def test_it_does_check_number_and_returns_true
-    assert_equal(MiniTestPractice::Calculator.check_number?("4004"), false)
-  end
-  
-  def test_it_return_false
-    assert_equal(MiniTestPractice::Calculator.check_number?("0334"), false)
-  end
-
-  def test_it_does_check_number_and_returns_false
-    assert_equal(MiniTestPractice::Calculator.check_number?(50334), false)
+  def test_it_does_check_merginal_number_and_returns_false
+    assert_equal(MiniTestPractice::Calculator.check_number?(10000), false)
+    assert_equal(MiniTestPractice::Calculator.check_number?(9999), false)
+    assert_equal(MiniTestPractice::Calculator.check_number?(1000), true)
+    assert_equal(MiniTestPractice::Calculator.check_number?(999), false)
   end
 
   def test_it_does_check_number_string
     assert_equal(MiniTestPractice::Calculator.check_number?("test"), false)
   end
 
-  def test_it_has_less_than_3_length
+  def test_it_has_less_then_3_length
     assert_equal(MiniTestPractice::Calculator.enough_length?("ee"), false)
+    assert_equal(MiniTestPractice::Calculator.enough_length?("eee"), true)
   end
 
   def test_it_has_more_then_8_length
-    assert_equal(MiniTestPractice::Calculator.enough_length?("eeggssaass"), false)
-  end
-
-  def test_it_has_enough_length
-    assert_equal(MiniTestPractice::Calculator.enough_length?("aassdd"), true)
+    assert_equal(MiniTestPractice::Calculator.enough_length?("aaaaeeeee"), false)
+    assert_equal(MiniTestPractice::Calculator.enough_length?("aaaaeeee"), true)
   end
 
   def test_it_divide_by_zero
@@ -49,16 +42,10 @@ class TestMiniTestPractice < MiniTest::Test
 
   def test_it_call_fizz
     assert_equal MiniTestPractice::Calculator.fizz_buzz(9), "Fizz"
-  end  
-
-  def test_it_call_buzz
     assert_equal MiniTestPractice::Calculator.fizz_buzz(5), "Buzz"
-  end
-
-  def test_it_call_fizzbuzz
     assert_equal MiniTestPractice::Calculator.fizz_buzz(15), "FizzBuzz"
-  end
-
+  end  
+  
   def test_it_print_hello
     assert_output(/hello/){ MiniTestPractice::Calculator.sayHello }
   end
