8cbcdd048df83b1c0a689eec5f6e42b3016f9b0e
Yusuke Ohashi <yusuke@junkpiano.me>
2014-11-20 21:10:01 +0900

    rename Base MiniTest class name.

--- test/test_mini_test_practice.rb
+++ test/test_mini_test_practice.rb
@@ -1,7 +1,6 @@
 require 'minitest_helper'
-require 'mini_test_practice'
 
-class TestMiniTestPractice < MiniTest::Unit::TestCase
+class TestMiniTestPractice < MiniTest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MiniTestPractice::VERSION
   end
